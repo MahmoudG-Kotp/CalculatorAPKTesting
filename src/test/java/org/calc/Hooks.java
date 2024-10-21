b73deca541bd31a91238ee08e3ae4b92499231ba
MahmoudGamal <31800978+MahmoudG-Kotp@users.noreply.github.com>
2024-10-21 21:21:57 +0300

    Update Hooks.java

--- src/test/java/org/calc/Hooks.java
+++ src/test/java/org/calc/Hooks.java
@@ -26,7 +26,7 @@ public class Hooks {
             capabilities.setCapability("platformName", "Android");
             capabilities.setCapability("appium:automationName", "UiAutomator2");
             capabilities.setCapability("appium:deviceName", "Medium Phone API 35");
-            capabilities.setCapability("appium:app", "C:\\Users\\mahmo\\Desktop\\Assignments\\appium\\twi_calculator_0.3.apk");
+            capabilities.setCapability("appium:app", "C:\\Users\\mahmo\\Desktop\\Assignments\\appium\\<Your APK>");
             //This line creates a new AndroidDriver instance, connecting to the Appium server on localhost (127.0.0.1)and port 4723, passing the previously configured options.
             try {
                 rootDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
